066a652b7a644cfe335bd9b15887fd6baa05da55
iampx95@gmail.com <iampx95@gmail.com>
2025-05-25 23:22:34 +0800

    added show arrivals button feature

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const arrivalInfo = document.getElementById("arrivalContent");
 
 // ----------------------obtain data from API-----------------------
 async function fetchBusArrivalInfo(busStopId) {
-    const response = await fetch('https://sg-bus-arrivals.vercel.app/?id=');
+    const response = await fetch(`https://sg-bus-arrivals.vercel.app/?id=${userBusStopId}`);
 
     if (response.ok) {
         const data = await response.json();
@@ -18,14 +18,48 @@ function busArrivalFormat(arrivalData) {
     const buses = arrivalData.services;
     const totalBuses = buses.length;
     const formattedData = [];
+
+    formattedData.push(`<p><strong>${totalBuses} buses</strong></p>`);
+
     for (const bus of buses) {
         const arrivalTimeString = bus.next_bus_mins <= 0 ? `<strong>Arriving</strong>` : `${bus.next_bus_mins} min(s)`;
+        
         formattedData.push(`
             <div>
-                <p><strong>${totalBuses} buses</strong></p>
+                <p>Bus <strong>${bus.no}</strong>: ${arrivalTimeString}</p>
             </div>
         `)
-
-        return formattedData.join(" ");
     }
+
+    return formattedData.join(" ");
+}
+// ---------------------------update content bus arrival data to HTML---------------------
+function displayBusArrival(busStopId) {
+    arrivalInfo.innerHTML = "Loading...";
+    fetchBusArrivalInfo(busStopId)
+        .then((arrivalData) => {
+            const formattedArrivalData = busArrivalFormat(arrivalData);
+            arrivalInfo.innerHTML = formattedArrivalData;
+        })
+        .catch((error) => {
+            console.error("Error", error);
+            arrivalInfo.innerHTML = 'Failed to load bus arrival data.';
+        })
+}   
+
+// ---------retrieves value of userBusStopId to display on webpage once button clicked-------
+let intervalId;
+
+function showArrivals() {
+    const busStopId = userBusStopId.value;
+
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+
+    displayBusArrival(busStopId);
+
+    intervalId = setInterval(() => {
+        displayBusArrival(busStopId);
+    }, 5000);
 }
